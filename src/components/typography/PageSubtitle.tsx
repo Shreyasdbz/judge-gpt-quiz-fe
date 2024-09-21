28bf5ff4b96ba3d5a56ae720ae253436f47dd018
Shreyas Sane <shreyasdbz@gmail.com>
2024-09-20 22:26:08 -0400

    about and privacy policy pages

---
+++ src/components/typography/PageSubtitle.tsx
@@ -0,0 +1,7 @@
+const PageSubtitle = ({ text }: { text: string }) => (
+  <h2 className="w-full max-w-6xl text-left text-xl font-semibold text-gray-900 dark:text-gray-100">
+    {text}
+  </h2>
+);
+
+export default PageSubtitle;
